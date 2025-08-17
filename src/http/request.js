e0e806bf7c3564236d6f24f52306c758cb0df215
DonnyHuo <huocheng122828@gmail.com>
2025-08-17 20:36:33 +0800

    feat: pro init

--- src/http/request.js
+++ src/http/request.js
@@ -2,8 +2,8 @@ import { request } from "graphql-request";
 
 export const fetchData = async (data) => {
   const endpoint =
-    "https://gateway.thegraph.com/api/subgraphs/id/ACEihynMMPkfPH9NG14o9vpmvNjK3kTC7Ce7ozEGmTA1";
+    "https://api.studio.thegraph.com/query/84515/xmhstaking/version/latest";
   return request(endpoint, data, undefined, {
-    Authorization: "Bearer 15e21550a1f515dd302fe54bf5635a40"
+    Authorization: "Bearer e83a48c59752df0e195cec37aec2f506"
   });
 };
